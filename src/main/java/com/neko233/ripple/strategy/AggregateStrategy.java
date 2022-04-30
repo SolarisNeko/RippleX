b3f27a339313ef7244e281148490aa6993112fdb
SolarisNeko <1417015340@qq.com>
2022-04-30 22:53:57 +0800

    1. [Update] code comment update.

--- src/main/java/com/neko233/ripple/strategy/AggregateStrategy.java
+++ src/main/java/com/neko233/ripple/strategy/AggregateStrategy.java
@@ -13,18 +13,16 @@ import java.util.function.BiFunction;
 public class AggregateStrategy {
 
     public static void aggregate(Map<String, Object> aggregateDataMap, Map<String, AggregateType> aggTypeMap, String aggColName, Object aggValue) {
+        // 1. get user AggregateType
         AggregateType aggType = aggTypeMap.get(aggColName);
         if (aggType == null) {
             return;
         }
-        String valueTypename = aggValue.getClass().getName();
-        if (valueTypename == null) {
-            return;
-        }
 
-        // 选择合并策略
+        // 2. get Merge Strategy
         MergeStrategy mergeStrategy = MergeStrategy.choose(aggType);
         BiFunction<? super Object, ? super Object, ?> merge = mergeStrategy.merge(aggValue.getClass());
+        // 3. COUNT is a special type
         if (aggType == AggregateType.COUNT) {
             aggregateDataMap.merge(aggColName, 1, merge);
         } else {
